24cc8086c48a8f7647f41c37ea5838b34164ad8b
rina <rina@precision3581-026.siren-koi.ts.net>
2023-08-22 14:37:21 +1000

    fish and plasma en_AU.

--- _zsh/.config/fish/config.fish
+++ _zsh/.config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    abbr -a -- zy 'sudo zypper'
+    abbr -a -- vim nvim
+    bind \b backward-kill-path-component
+    bind \e\[3\;5\~ kill-word
 end
