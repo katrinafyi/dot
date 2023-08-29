a3c9b06c1a202db36378f58a48b1e3802d73bcdc
rina <k@rina.fyi>
2023-08-29 12:08:25 +1000

    gitconfig.

--- _zsh/.config/fish/config.fish
+++ _zsh/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr -a -- zy 'sudo zypper'
     abbr -a -- vim nvim
+    abbr -a -- g git
     bind \b backward-kill-path-component
     bind \e\[3\;5\~ kill-word
 end
