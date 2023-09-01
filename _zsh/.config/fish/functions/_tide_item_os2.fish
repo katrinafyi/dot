6ae41aa229dd60bf4544bf4186e3ee5c8672ba30
rina <k@rina.fyi>
2023-09-01 21:26:45 +1000

    os icon.

---
+++ _zsh/.config/fish/functions/_tide_item_os2.fish
@@ -0,0 +1,8 @@
+function _tide_item_os2
+    _tide_detect_os | read -g --line os_branding_icon os_branding_color os_branding_bg_color
+    set -U tide_os_bg_color normal
+    set -U tide_os_color $os_branding_color
+    set -U tide_os_icon $os_branding_icon
+
+    _tide_print_item os $tide_os_icon
+end
