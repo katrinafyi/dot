bc6ac698d09aff5e87025416542b1959657349b2
rina <k@rina.fyi>
2023-09-02 13:05:16 +1000

    os in config.fish

--- _zsh/.config/fish/config.fish
+++ _zsh/.config/fish/config.fish
@@ -6,4 +6,9 @@ if status is-interactive
     bind \b backward-kill-path-component
     bind \e\[3\;5\~ kill-word
     bind \cZ echo fg
+
+    _tide_detect_os | read -g --line os_branding_icon os_branding_color os_branding_bg_color
+    set -U tide_os_bg_color normal
+    set -U tide_os_color white
+    set -U tide_os_icon $os_branding_icon
 end
