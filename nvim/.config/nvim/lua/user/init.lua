827541d1dc219359d8e382ab19d36a10790c2b54
rina <k@rina.fyi>
2024-01-18 12:25:42 +1000

    vimtex

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -3,6 +3,13 @@ return {
     vim.cmd('highlight VertSplit guifg=Grey')
     vim.cmd('highlight LspCodeLens guifg=#626a73')
     vim.cmd('highlight! link LspInlayHint LspCodeLens')
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = {"tex", "*.tex", "*.txt", "*.md"},
+      callback = function(ev)
+        vim.opt_local.wrap = true
+      end
+    })
   end,
   colorscheme = "ayu",
 
