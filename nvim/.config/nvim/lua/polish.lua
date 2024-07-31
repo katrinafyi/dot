a9a9e5d527c86f543b0306672365783177c8aa01
rina <k@rina.fyi>
2024-07-31 16:52:18 +1000

    polish

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -1,18 +1,63 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+-- return {
+  -- polish = function()
+    vim.cmd('highlight VertSplit guifg=Grey')
+    vim.cmd('highlight WinSeparator guifg=Grey')
+    vim.cmd('highlight LspCodeLens guifg=#626a73')
+    vim.cmd('highlight! link LspInlayHint LspCodeLens')
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = {"tex", "*.tex", "*.txt", "*.md"},
+      callback = function(ev)
+        vim.opt_local.wrap = true
+      end
+    })
+
+    vim.keymap.set('n', 'S', function ()
+      require('leap').leap { target_windows = { vim.api.nvim_get_current_win() } }
+    end)
+  -- end,
+
+
+--   colorscheme = "ayu",
+--
+--   lsp = {
+--     formatting = {
+--       format_on_save = false,
+--     },
+--     servers = {
+--       "clangd",
+--     },
+--     config = {
+--       html = {
+--         settings = {
+--           autoClosingTags = true,
+--           format = {
+--             templating = true,
+--           },
+--           hover = {
+--             documentation = true,
+--             references = true,
+--           },
+--         }
+--       }
+--     }
+--   },
+-- }
