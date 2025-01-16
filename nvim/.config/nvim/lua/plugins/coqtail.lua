2c1baa2216e41f69c57ec65cd35f6185a20660b4
rina <k@rina.fyi>
2025-01-16 21:01:25 +1000

    fish completion helper

--- nvim/.config/nvim/lua/plugins/coqtail.lua
+++ nvim/.config/nvim/lua/plugins/coqtail.lua
@@ -1,6 +1,7 @@
 return {
   { 
     "whonore/Coqtail",
+    branch = "checked-region-autocmd",
     init = function ()
       vim.api.nvim_create_autocmd("FileType", {
         desc = "Coq keybindings",
@@ -11,6 +12,32 @@ return {
         end,
         pattern = "coq",
       })
+
+      -- vim.g.asdf = false;
+      -- vim.api.nvim_create_autocmd("User", {
+      --   pattern = "CoqCommandComplete",
+      --   desc = "Coqtail follow checked",
+      --   callback = function(event)
+      --     if not vim.g.asdf then
+      --       vim.g.asdf = true
+      --       vim.cmd "CoqJumpToEnd"
+      --       vim.g.asdf = false
+      --     end
+      --   end,
+      -- })
+      --
+      -- vim.api.nvim_exec([[
+		    -- let g:coqautocommanding = v:false
+      --   augroup CoqtailFollowChecked
+		    --   autocmd!
+		    --   autocmd! User CoqCommandComplete
+		    --     \  if ! g:coqautocommanding && g:coqtail#event.cmd =~# 'step\|rewind\|to_line\|to_top'
+		    --     \|   let g:coqautocommanding = v:true
+			   --    \|   CoqJumpToEnd
+		    --     \|   let g:coqautocommanding = v:false
+			   --    \| endif
+		    -- augroup END
+      -- ]], false)
     end,
   }
 }
